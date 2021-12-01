7d220ecc9fdebbea8fe850d44dad2b29c14a9168
Péter Szász <eszpee@gmail.com>
2021-12-01 12:19:59 +0100

    move foreach function out

--- 01/increases1.js
+++ 01/increases1.js
@@ -1,13 +1,15 @@
 'use strict';
-let increases = 0;
-let prev = undefined;
-
-require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(function(line){
+const measurer = (line) => {
     let depth = parseInt(line);
     if ((prev != undefined) && (depth > prev)) {
         increases++;
     }
     prev = depth;
-});
+};
+
+let increases = 0;
+let prev = undefined;
+
+require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(measurer);
 
 console.log(increases);
--- 01/increases2.js
+++ 01/increases2.js
@@ -5,10 +5,7 @@ function compare(arr) {
     return (arr.slice(1,4).reduce(sum) > arr.slice(0,3).reduce(sum));
 } 
 
-let measures = new Array();
-let increases = 0;
-
-require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(function(line){
+const measurer = (line) => {
     measures.push(parseInt(line));
     if (measures.length >= 4) {
         if (compare(measures)) {
@@ -16,6 +13,11 @@ require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt
         }
         measures.shift();
     }
-});
+};
+
+let measures = new Array();
+let increases = 0;
+
+require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(measurer);
 
 console.log(increases);

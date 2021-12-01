c00973507721836a8110a9227e28eb16be2a6b41
Péter Szász <eszpee@gmail.com>
2021-12-01 11:53:32 +0100

    naive solution to challenge 1b

--- 01/increases2.js
+++ 01/increases2.js
@@ -1,12 +1,33 @@
 'use strict';
+function compare(arr) {
+    let sum1=0;
+    let sum2=0;
+    for (let i=0; i<=2; i++) {
+        sum1 += arr[i];
+    }
+    for (let i=1; i<=3; i++) {
+        sum2 += arr[i];
+    }
+    if (sum2 > sum1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
+} 
+
+let measures = new Array();
+let increases = 0;
 
 require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(function(line){
-    let depth = parseInt(line);
-    if ((prev != undefined) && (depth > prev)) {
-        increases++;
+    measures.push(parseInt(line));
+    if (measures.length >= 4) {
+        if (compare(measures)) {
+            increases++;
+        }
+        measures.shift();
     }
-    prev = depth;
 });
 
 console.log(increases);

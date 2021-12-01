8bccfa5c32051aab09e933a1a6b618a848cafaec
Péter Szász <eszpee@gmail.com>
2021-12-01 12:26:08 +0100

    solution with .reduce() - thx marci

--- 01/increases1.js
+++ 01/increases1.js
@@ -1,15 +1,14 @@
 'use strict';
-const measurer = (line) => {
-    let depth = parseInt(line);
-    if ((prev != undefined) && (depth > prev)) {
+const measurer = (prev,curr) => {
+    let depth = parseInt(curr);
+    if (depth > parseInt(prev)) {
         increases++;
     }
-    prev = depth;
+    return depth;
 };
 
 let increases = 0;
-let prev = undefined;
 
-require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).forEach(measurer);
+require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/01/input.txt', 'utf-8').split(/\r?\n/).reduce(measurer);
 
 console.log(increases);

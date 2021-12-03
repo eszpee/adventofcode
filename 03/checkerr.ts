1539e588f14baf4f9822ecc32f769cc97b17e2ab
Péter Szász <eszpee@gmail.com>
2021-12-03 09:57:49 +0100

    solution for day 3a

---
+++ 03/checkerr.ts
@@ -0,0 +1,19 @@
+'use strict';
+let input = new Array();
+input = require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/03/input.txt', 'utf-8').split(/\r?\n/);
+let sum = new Array(input[0].length).fill(0);
+for (let i:number = 0; i<input.length; i++) {
+    let line:string[] = input[i].split('');
+    line.forEach((digit:string, index:number) => {sum[index]+= parseInt(digit);})
+}
+let result:string = '';
+sum.forEach((item:number) => {
+    if (item < (input.length/2)) {
+        result += '0';
+    } else {
+        result += '1';
+    }
+});
+const errs:number = parseInt(result,2);
+const inv:number = errs^Math.pow(2,input[0].length)-1;
+console.log(errs*inv);

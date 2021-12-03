3fe828600d4079d521af2cfe67619fab5a5bcbf1
Péter Szász <eszpee@gmail.com>
2021-12-03 10:07:10 +0100

    npm packages cleanup

--- 03/checkerr.js
+++ 03/checkerr.js
@@ -1,6 +1,6 @@
 'use strict';
 var input = new Array();
-input = require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/03/input.txt', 'utf-8').split(/\r?\n/);
+input = require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/03/input_small.txt', 'utf-8').split(/\r?\n/);
 var sum = new Array(input[0].length).fill(0);
 for (var i = 0; i < input.length; i++) {
     var line = input[i].split('');
@@ -15,6 +15,7 @@ sum.forEach(function (item) {
         result += '1';
     }
 });
+console.log(sum, result);
 var errs = parseInt(result, 2);
 var inv = errs ^ Math.pow(2, input[0].length) - 1;
 console.log(errs * inv);
--- 03/checkerr.ts
+++ 03/checkerr.ts
@@ -1,6 +1,6 @@
 'use strict';
 let input = new Array();
-input = require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/03/input.txt', 'utf-8').split(/\r?\n/);
+input = require('fs').readFileSync('/Users/eszpee/projects/adventofcode2021/03/input_small.txt', 'utf-8').split(/\r?\n/);
 let sum = new Array(input[0].length).fill(0);
 for (let i:number = 0; i<input.length; i++) {
     let line:string[] = input[i].split('');
@@ -14,6 +14,7 @@ sum.forEach((item:number) => {
         result += '1';
     }
 });
+console.log(sum,result);
 const errs:number = parseInt(result,2);
 const inv:number = errs^Math.pow(2,input[0].length)-1;
 console.log(errs*inv);

65b2083540afb01235a06261b4434fadf8d8b3b3
Péter Szász <eszpee@gmail.com>
2021-12-04 14:33:01 +0100

    Sample solution with lots of debug info

---
+++ src/day04b.js
@@ -0,0 +1,118 @@
+"use strict";
+exports.__esModule = true;
+var BingoBoard = /** @class */ (function () {
+    function BingoBoard(board) {
+        this.size = board[0].length;
+        this.board = board;
+        this.checked = new Array(this.size);
+        for (var i = 0; i < this.size; i++) {
+            this.checked[i] = new Array(this.size).fill(false);
+        }
+    }
+    BingoBoard.prototype.crossOut = function (num) {
+        var found = false;
+        for (var i = 0; i < this.size; i++) {
+            for (var j = 0; j < this.size; j++) {
+                if (this.board[i][j] === num) {
+                    this.checked[i][j] = true;
+                    found = true;
+                }
+            }
+        }
+        return found;
+    };
+    BingoBoard.prototype.didWeWin = function () {
+        for (var i = 0; i < this.size; i++) {
+            // check horizontal
+            var rowFull = true;
+            var j = 0;
+            while ((rowFull) && (j < this.size)) {
+                if (!this.checked[i][j]) {
+                    rowFull = false;
+                }
+                j++;
+            }
+            if (rowFull) {
+                return true;
+            }
+            // check vertical
+            var colFull = true;
+            j = 0;
+            while ((colFull) && (j < this.size)) {
+                if (!this.checked[j][i]) {
+                    colFull = false;
+                }
+                j++;
+            }
+            if (colFull) {
+                return true;
+            }
+        }
+        return false;
+    };
+    BingoBoard.prototype.remainingSum = function () {
+        var sum = 0;
+        for (var i = 0; i < this.size; i++) {
+            for (var j = 0; j < this.size; j++) {
+                if (!this.checked[i][j]) {
+                    sum += this.board[i][j];
+                }
+            }
+        }
+        return sum;
+    };
+    return BingoBoard;
+}());
+function boardsStillPlaying(boards) {
+    var noBingoHere = new Array();
+    for (var i = 0; i < boards.length; i++) {
+        if (!boards[i].didWeWin()) {
+            noBingoHere.push(boards[i]);
+        }
+    }
+    return noBingoHere;
+}
+var inputfile_1 = require("./inputfile");
+var inputArray = (0, inputfile_1.readInputArray)('../input/day04_sample.txt');
+var boards = new Array();
+var numbers = inputArray.shift().split(',').map(function (v) { return parseInt(v, 10); });
+var line = '';
+var tempArray = new Array();
+while ((line = inputArray.shift()) !== undefined) {
+    if (line === '') {
+        if (tempArray.length > 0) {
+            boards.push(new BingoBoard(tempArray));
+        }
+        tempArray = new Array();
+    }
+    else {
+        if (line.charAt(0) === ' ') {
+            line = line.substr(1);
+        }
+        tempArray.push(line.split(/  ?/).map(function (v) { return parseInt(v, 10); }));
+    }
+}
+boards.push(new BingoBoard(tempArray));
+var weHaveAWinner = false;
+for (var numIndex = 0; !weHaveAWinner && numIndex < numbers.length; numIndex++) {
+    //reading out numbers
+    var currentNumber = numbers[numIndex];
+    console.log("new number read out: ".concat(currentNumber));
+    for (var b = 0; b < boards.length; b++) {
+        // let's cross out this number on all boards first, before we check for winners.
+        boards[b].crossOut(currentNumber);
+    }
+    for (var b = 0; b < boards.length; b++) {
+        if (boards[b].didWeWin()) {
+            console.log("board number ".concat(b, " won with number ").concat(currentNumber));
+            console.log('winning board:', boards[b]);
+            if (boards.length == 1) { //this was the last board!
+                weHaveAWinner = true;
+                console.log(currentNumber * boards[b].remainingSum());
+                break;
+            }
+            boards.splice(b, 1); //remove winning board
+            console.log('boards remaining:', boards);
+        }
+    }
+}
---
+++ src/day04b.ts
@@ -0,0 +1,123 @@
+class BingoBoard {
+    size: number;
+    board: number[][];
+    checked: boolean[][];
+
+    constructor(board: number[][]) {
+        this.size = board[0].length;
+        this.board = board;
+        this.checked = new Array(this.size);
+        for (let i = 0; i<this.size; i++) {
+            this.checked[i] = new Array(this.size).fill(false);
+        }
+    }
+
+    crossOut(num: number):boolean {
+        let found = false;
+        for (let i:number=0;i<this.size;i++) {
+            for (let j:number=0;j<this.size;j++) {
+                if (this.board[i][j] === num) {
+                    this.checked[i][j] = true;
+                    found = true;
+                }
+            }
+        }
+        return found;
+    }
+
+    didWeWin():boolean {
+        for (let i:number=0;i<this.size;i++) {
+            // check horizontal
+            let rowFull:boolean = true;
+            let j:number = 0;
+            while ((rowFull) && (j<this.size)) {
+                if (!this.checked[i][j]) {
+                    rowFull = false;
+                }
+                j++;
+            }
+            if(rowFull) {return true;}
+
+            // check vertical
+            let colFull:boolean = true;
+            j=0;
+            while ((colFull) && (j<this.size)) {
+                if (!this.checked[j][i]) {
+                    colFull = false;
+                }
+                j++;
+            }
+            if(colFull) {return true;}
+        }
+        return false;
+    }
+
+    remainingSum():number {
+        let sum:number = 0;
+        for (let i:number=0;i<this.size;i++) {
+            for (let j:number=0;j<this.size;j++) {
+                if (!this.checked[i][j]) {
+                    sum += this.board[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+}
+
+function boardsStillPlaying(boards:BingoBoard[]):BingoBoard[] {
+    let noBingoHere:BingoBoard[] = new Array();
+    for (let i:number=0; i<boards.length;i++) {
+        if (!boards[i].didWeWin()) {
+            noBingoHere.push(boards[i]);
+        }
+    }
+    return noBingoHere;
+}
+
+import { readInputArray } from "./inputfile";
+const inputArray:string[] = readInputArray('../input/day04_sample.txt');
+let boards:BingoBoard[] = new Array();
+
+let numbers:number[] = inputArray.shift().split(',').map((v) => parseInt(v,10));
+
+let line:string|undefined = '';
+let tempArray = new Array();
+while ((line = inputArray.shift()) !== undefined) {
+    if (line === '') {
+        if (tempArray.length > 0) {
+            boards.push(new BingoBoard(tempArray));
+        }
+        tempArray = new Array();
+    }
+    else {
+        if (line.charAt(0) === ' ') { line = line.substr(1); }
+        tempArray.push(line.split(/  ?/).map((v) => parseInt(v,10)));
+    }
+}
+boards.push(new BingoBoard(tempArray));
+
+let weHaveAWinner:boolean = false;
+for (let numIndex = 0;!weHaveAWinner && numIndex<numbers.length; numIndex++) {
+    //reading out numbers
+    let currentNumber = numbers[numIndex];
+    console.log(`new number read out: ${currentNumber}`);
+    for (let b = 0;b<boards.length;b++) {
+        // let's cross out this number on all boards first, before we check for winners.
+        boards[b].crossOut(currentNumber);
+    }
+    for (let b = 0;b<boards.length;b++) {
+        if (boards[b].didWeWin()) {
+            console.log(`board number ${b} won with number ${currentNumber}`);
+            console.log('winning board:',boards[b]);
+            if (boards.length == 1) { //this was the last board!
+                weHaveAWinner = true;
+                console.log(currentNumber*boards[b].remainingSum());
+                break;
+            }
+            boards.splice(b,1); //remove winning board
+            console.log('boards remaining:',boards);
+        }
+    }
+}

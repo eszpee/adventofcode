44a67dc0a25771c63057a92162658d468f41f7f7
Péter Szász <eszpee@gmail.com>
2021-12-04 12:55:07 +0100

    BingoBoard class implementation

---
+++ src/day04.js
@@ -0,0 +1,69 @@
+var BingoBoard = /** @class */ (function () {
+    function BingoBoard(board) {
+        this.size = board[0].length;
+        this.board = board;
+        this.checked = new Array(this.size);
+        for (var i = 0; i < this.size; i++) {
+            this.checked[i] = new Array(this.size).fill(false);
+        }
+    }
+    BingoBoard.prototype.crossOut = function (num) {
+        var found = false;
+        for (var i = 0; i < this.size; i++) {
+            for (var j = 0; j < this.size; j++) {
+                if (this.board[i][j] === num) {
+                    this.checked[i][j] = true;
+                    found = true;
+                }
+            }
+        }
+        return found;
+    };
+    BingoBoard.prototype.didWeWin = function () {
+        for (var i = 0; i < this.size; i++) {
+            // check horizontal
+            var rowFull = true;
+            var j = 0;
+            while ((rowFull) && (j < this.size)) {
+                if (!this.checked[i][j]) {
+                    rowFull = false;
+                }
+                j++;
+            }
+            if (rowFull) {
+                return true;
+            }
+            // check vertical
+            var colFull = true;
+            j = 0;
+            while ((colFull) && (j < this.size)) {
+                if (!this.checked[j][i]) {
+                    colFull = false;
+                }
+                j++;
+            }
+            if (colFull) {
+                return true;
+            }
+        }
+        return false;
+    };
+    BingoBoard.prototype.remainingSum = function () {
+        var sum = 0;
+        for (var i = 0; i < this.size; i++) {
+            for (var j = 0; j < this.size; j++) {
+                if (!this.checked[i][j]) {
+                    sum += this.board[i][j];
+                }
+            }
+        }
+        return sum;
+    };
+    return BingoBoard;
+}());
+var testField = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+var testBoard = new BingoBoard(testField);
+testBoard.crossOut(5);
+testBoard.crossOut(2);
+testBoard.crossOut(8);
+console.log(testBoard, testBoard.didWeWin(), testBoard.remainingSum());

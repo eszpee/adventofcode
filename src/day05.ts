f57d579219382c2a0381a6df6ea08ff9c0dc4cd4
Péter Szász <eszpee@gmail.com>
2021-12-05 10:06:56 +0100

    Solution for sample dataset implemented (maxes removed, misunderstood specification first...)

--- src/day05.ts
+++ src/day05.ts
@@ -1,14 +1,14 @@
 class SeaMap {
     fields: number[][]; //2D map, 0-no current
-    maxes: number[];    //maxes[3]: how many points on map with 3 lines crossing  //we probably won't need this damn it
+//    maxes: number[];    //maxes[3]: how many points on map with 3 lines crossing  //we probably won't need this damn it
     size: number;       //let's consider the map square for now
 
-    constructor(size:number) {
-        this.size = size;
-        this.maxes = new Array(255).fill(0);     //hope no more than 255 lines crossing at one point
-        this.fields = new Array(size);
-        for (let i=0;i<size;i++) {
-            this.fields[i] = new Array(size).fill(0);
+    constructor(s:number|undefined) {
+        this.size = s == undefined ? 0 : s+1;
+//        this.maxes = new Array(255).fill(0);     //hope no more than 255 lines crossing at one point
+        this.fields = new Array(this.size);
+        for (let i=0;i<this.size;i++) {
+            this.fields[i] = new Array(this.size).fill(0);
         }
     }
 
@@ -25,23 +25,29 @@ class SeaMap {
 
     drawLine(a:number[],b:number[]):void {
         if (a[0] === b[0]) {
+            if (a[1]>b[1]) { //need to swap coordinates to draw left->right, up->down
+                [a[1],b[1]] = [b[1],a[1]];
+            }
             for (let i=a[1]; i<=b[1];i++) {
-                this.fields[a[0]][i]++;
-                this.maxes[this.fields[a[0]][i]]++;
+                this.fields[i][a[0]]++;
+//                this.maxes[this.fields[a[0]][i]]++;
             }
         }
         else if (a[1] === b[1]) {
+            if (a[0]>b[0]) { //need to swap coordinates to draw left->right, up->down
+                [a[0],b[0]] = [b[0],a[0]];
+            }
             for (let i=a[0]; i<=b[0];i++) {
-                this.fields[i][a[1]]++;
-                this.maxes[this.fields[i][a[1]]]++;
+                this.fields[a[1]][i]++;
+//               this.maxes[this.fields[i][a[1]]]++;
             }
         }
         else {
-            throw('Diagonal lines not implemented!');
+            ;// Diagonal lines not implemented for now
         }
     }
 
-    getMax():number { 
+/*     getMax():number { 
         for (let i=this.maxes.length;i>0;i--) {
             if (this.maxes[i] >0) {
                 return i;
@@ -49,7 +55,7 @@ class SeaMap {
         }
         return NaN;
     }
-
+ */
     getCrosses():number {
         //find the number of points where more than one lines overlap
         let crosses = 0;
@@ -64,18 +70,43 @@ class SeaMap {
     }
 
 }
-/* 
+
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day05_sample.txt');
-let maxSize = 0;
-let line:string|undefined = '';
-while ((line = inputArray.shift()) !== undefined) {
-    let coords = line.split(' -> ').map(x => x.split(','));
-    console.log(coords);
-}
- */
-let myMap= new SeaMap(10);
+
+//determine map size. TODO: refactor SeaMap to have flexible map sizes...
+const maxCoord:number | undefined = inputArray.map(x =>
+    x.split(' -> ').map(x =>
+        x.split(',')).map(x => {
+            return x.map(y => {
+                return parseInt(y, 10)
+            })
+        })).flat(2).sort((a, b) => {
+            return a - b;
+        }).pop();
+
+let myMap= new SeaMap(maxCoord);
+
+
+inputArray.map(x =>
+    x.split(' -> ').map(x =>
+        x.split(',')).map(x => {
+            return x.map(y => {
+                return parseInt(y, 10)
+            })
+        }
+    )
+).forEach(command => {
+    myMap.drawLine(command[0],command[1]);
+});
+
+myMap.printMap();
+console.log(myMap.getCrosses());
+
+
+/* let myMap= new SeaMap(10);
 myMap.drawLine([0,0],[0,7]);
 myMap.drawLine([0,5],[9,5]);
 myMap.printMap();
 console.log(myMap.getCrosses());
+ */

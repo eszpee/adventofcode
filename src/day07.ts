028f5f52269d5ad3ec5b10f81ccd6120cf573175
Péter Szász <eszpee@gmail.com>
2021-12-07 10:52:42 +0100

    This is even uglier but I got lost in map/reduces, so oldschool for loop. Can't check because site's down.

--- src/day07.ts
+++ src/day07.ts
@@ -1,11 +1,23 @@
 import { readInputArray } from "./inputfile";
-const inputArray:string[] = readInputArray('../input/day07_sample.txt');
+const inputArray:string[] = readInputArray('../input/day07.txt');
 const positions = inputArray[0].split(',').map(s=>parseInt(s,10));
 function calculateConstantFuel(items:number[],target:number):number {
     //calculates the combined fuel needed for all items in ${positions} to get to ${target}
     return items
         .map(item => Math.abs(target-item))
         .reduce((p,c) => p+c);
+}
+function calculateExponentialFuel(items:number[],target:number):number {
+    //calculates the combined fuel needed for all items in ${positions} to get to ${target}
+    let sum = 0;
+    for (let i=0;i<items.length;i++) {
+        //iterate all items 
+        const item = items[i];
+        const distance = Math.abs(item-target);
+        const steps = distance * ((distance + 1) / 2);
+        sum += steps;
+    }
+    return sum;
 }
 function arrayMax(arr:number[]):number {
     //return max item of array
@@ -29,5 +41,13 @@ for (let i=0;i<=arrayMax(positions);i++) {
     distancesConst.push(dist);
 }
 
+const distancesExp:number[] = new Array();
+for (let i=0;i<=arrayMax(positions);i++) {
+    const dist = calculateExponentialFuel(positions,i);
+//    console.log(`Target is ${i}, combined fuel needed to get there: `,dist);
+    distancesExp.push(dist);
+}
+
 console.log("Constant burning fuel needed to get to target: ",arrayMin(distancesConst));
+console.log("Exponential burning fuel needed to get to target: ",arrayMin(distancesExp));
 

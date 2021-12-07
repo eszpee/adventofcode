483944400b9661af02aa11d4cbfa1ac52fa91743
Péter Szász <eszpee@gmail.com>
2021-12-07 09:04:32 +0100

    Sample solution

--- src/day07.ts
+++ src/day07.ts
@@ -1,3 +1,18 @@
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day07_sample.txt');
 
+const average = Math.round(
+    inputArray[0]
+        .split(',')
+        .map(s=>parseInt(s,10))
+        .reduce((prev, curr) => prev+curr) 
+    / inputArray[0].length);
+
+console.log(`Point to get to: ${average}`);
+
+const cumulativeDistanceFromAverage = inputArray[0]
+    .split(',')
+    .map(x => Math.abs(average - parseInt(x,10)))
+    .reduce((p,c) => p+c);
+
+console.log(`Combined fuel needed to get there: ${cumulativeDistanceFromAverage}`);

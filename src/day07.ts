5a26b3f474516a65399dc1a588307c39ffc4ba7e
Péter Szász <eszpee@gmail.com>
2021-12-07 13:19:23 +0100

    cleanup

--- src/day07.ts
+++ src/day07.ts
@@ -1,23 +1,18 @@
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day07.txt');
 const positions = inputArray[0].split(',').map(s=>parseInt(s,10));
+
 function calculateConstantFuel(items:number[],target:number):number {
-    //calculates the combined fuel needed for all items in ${positions} to get to ${target}
+    //calculates the combined constant fuel needed for all items in ${positions} to get to ${target}
     return items
         .map(item => Math.abs(target-item))
         .reduce((p,c) => p+c);
 }
 function calculateExponentialFuel(items:number[],target:number):number {
-    //calculates the combined fuel needed for all items in ${positions} to get to ${target}
-    let sum = 0;
-    for (let i=0;i<items.length;i++) {
-        //iterate all items 
-        const item = items[i];
-        const distance = Math.abs(item-target);
-        const steps = distance * ((distance + 1) / 2);
-        sum += steps;
-    }
-    return sum;
+    //calculates the combined exponential fuel needed for all items in ${positions} to get to ${target}
+    return items
+        .map(item => Math.abs(item-target) * ((Math.abs(item-target) + 1) / 2))
+        .reduce((p,c) => p+c);
 }
 function arrayMax(arr:number[]):number {
     //return max item of array
@@ -34,20 +29,15 @@ const average = Math.round(
     positions.length
 );
 
-const distancesConst:number[] = new Array();
-for (let i=0;i<=arrayMax(positions);i++) {
-    const dist = calculateConstantFuel(positions,i);
-//    console.log(`Target is ${i}, combined fuel needed to get there: `,dist);
-    distancesConst.push(dist);
-}
+console.log("Constant burning fuel needed to get to target: ",
+    arrayMin(
+        positions.map(i => calculateConstantFuel(positions,i))
+    )
+);
 
-const distancesExp:number[] = new Array();
-for (let i=0;i<=arrayMax(positions);i++) {
-    const dist = calculateExponentialFuel(positions,i);
-//    console.log(`Target is ${i}, combined fuel needed to get there: `,dist);
-    distancesExp.push(dist);
-}
-
-console.log("Constant burning fuel needed to get to target: ",arrayMin(distancesConst));
-console.log("Exponential burning fuel needed to get to target: ",arrayMin(distancesExp));
+console.log("Exponential burning fuel needed to get to target: ",
+    arrayMin(
+        positions.map(i => calculateExponentialFuel(positions,i))
+    )
+);
 

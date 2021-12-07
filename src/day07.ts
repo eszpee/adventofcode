1cff7b420947291436f92d2a8b1f2a79e0964d34
Péter Szász <eszpee@gmail.com>
2021-12-07 10:02:32 +0100

    AMAZINGLY ugly solution for sample, as previous one didn't work on real data :(

--- src/day07.ts
+++ src/day07.ts
@@ -1,18 +1,34 @@
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day07_sample.txt');
+const positions = inputArray[0].split(',').map(s=>parseInt(s,10));
+console.log(positions);
+function calculateFuel(items:number[],target:number):number {
+    //calculates the combined fuel needed for all items in ${positions} to get to ${target}
+    return items
+        .map(item => Math.abs(target-item))
+        .reduce((p,c) => p+c);
+}
+function arrayMax(arr:number[]):number {
+    //return max item of array
+    return arr.reduce((prev,curr) => Math.max(prev,curr));
+}
+function arrayMin(arr:number[]):number {
+    //return min item of array
+    return arr.reduce((prev,curr) => Math.min(prev,curr));
+}
 
 const average = Math.round(
-    inputArray[0]
-        .split(',')
-        .map(s=>parseInt(s,10))
-        .reduce((prev, curr) => prev+curr) 
-    / inputArray[0].length);
+    positions.reduce((prev, curr) => prev+curr) 
+    / 
+    positions.length
+);
 
-console.log(`Point to get to: ${average}`);
+const distances:number[] = new Array();
+for (let i=0;i<=arrayMax(positions);i++) {
+    const dist = calculateFuel(positions,i);
+//    console.log(`Target is ${i}, combined fuel needed to get there: `,dist);
+    distances.push(dist);
+}
 
-const cumulativeDistanceFromAverage = inputArray[0]
-    .split(',')
-    .map(x => Math.abs(average - parseInt(x,10)))
-    .reduce((p,c) => p+c);
+console.log("Fuel needed to get to target: ",arrayMin(distances));
 
-console.log(`Combined fuel needed to get there: ${cumulativeDistanceFromAverage}`);

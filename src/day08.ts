f8e186612d03d0ad1dd6bf249b263e79f07de9cf
Péter Szász <eszpee@gmail.com>
2021-12-08 10:27:37 +0100

    Part 1 solved.

--- src/day08.ts
+++ src/day08.ts
@@ -1,5 +1,5 @@
 import { readInputArray } from "./inputfile";
-const inputArray:string[] = readInputArray('../input/day08_sample.txt');
+const inputArray:string[] = readInputArray('../input/day08.txt');
 const inputPatterns:string[][] = new Array();
 const inputDigits:string[][] = new Array();
 inputArray.forEach(oneline => {
@@ -9,4 +9,18 @@ inputArray.forEach(oneline => {
     }
 );
 
-console.log(inputPatterns,inputDigits);
+function is1478(s:string):boolean {
+    //checks if the passed string represents 1,4,7 or 8 
+    //by looking at the length of the string, as ONLY these digits require 2,4,3,7 segments respectively
+    const goodLength = new Set([2,4,3,7]);
+    return goodLength.has(s.length);
+}
+
+//console.log(inputPatterns,inputDigits);
+let numberOfUniques = 0;
+inputDigits.forEach(oneline => {
+    oneline.forEach(oneitem => {
+        if (is1478(oneitem)) { numberOfUniques++; }
+    })
+});
+console.log(`${numberOfUniques} unique length segmentlists.`);

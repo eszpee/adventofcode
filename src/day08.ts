d6da8a7bb8fc98dc183994578c3384a4ac92e16d
Péter Szász <eszpee@gmail.com>
2021-12-08 21:01:39 +0100

    Still a lot to clean

--- src/day08.ts
+++ src/day08.ts
@@ -17,7 +17,7 @@ interface SegmentDecoder {
 interface DigitDecoder {
     [key: string]: number;
 }
-const numbers:DigitDecoder = {
+const originalNumbers:DigitDecoder = {
     'abcefg': 0, 
     'cf': 1,
     'acdeg': 2,
@@ -30,7 +30,7 @@ const numbers:DigitDecoder = {
     'abcdfg': 9
 };
 function doMagic(s:string[]):SegmentDecoder {
-    // deducts which segment represents which one 
+    // deducts which segment represents which one and returns a segment lookup object
     function nLength(s:string[],n:number):string[] {
         //returns the items in s[] that have a length of n
         let r:string[] = new Array();
@@ -48,15 +48,19 @@ function doMagic(s:string[]):SegmentDecoder {
         return np;
     }
 
-    let sd:SegmentDecoder = { //this will be filled up runtime
-        'd': 'X',//'A',
-        'e': 'X',//'B',
-        'a': 'X',//'C',
-        'f': 'X',//'D',
-        'g': 'X',//'E',
-        'b': 'X',//'F',
-        'c': 'X',//'G',
-    };
+    function otherOne(first:string,pair:string[]):string { 
+        //takes an item and a two-item array and returns the other item from it
+        return pair[1-pair.indexOf(first)];
+    }
+
+    function removeChars(chars:string[],s:string):string {
+        //removes all chars[] characters from string and returns remainder
+        const sArr = s.split('');
+        chars.forEach(chr => sArr.splice(sArr.indexOf(chr),1).join(''));
+        return sArr.join('');
+    }
+
+    let sd:SegmentDecoder = {};
 
     s = s.map(x=>x.split('').sort().join(''));
 
@@ -68,25 +72,18 @@ function doMagic(s:string[]):SegmentDecoder {
     //get the string that's 5-char long and includes ACF... 
     let three = '';
     let acf = [a,...cf];
-    s.forEach(string => {
-        if (string.length === 5) {
-            let contains = 0;
-            acf.forEach(chr => {if (string.match(chr)){contains++}});
-            if (contains === 3) {
-                three = string;
-            }
+    nLength(s,5).forEach(string => {
+        let contains = 0;
+        acf.forEach(chr => {if (string.match(chr)){contains++}});
+        if (contains === 3) {
+            three = string;
         }
     });
     //... and if we remove ACF then we have DG
-    const threeArr:string[] = three.split('');
-    acf.forEach(chr => threeArr.splice(threeArr.indexOf(chr),1));
-    let dg = threeArr;
+    let dg = removeChars(acf,three).split('');
 
     //get the string that's 4-char long (only one:4) and check which one of DG isn't in it => that should be D
-    let four = ''
-    s.forEach(string => {
-        if (string.length === 4) {four = string;}
-    })
+    const four = nLength(s,4)[0];
     let d = '';
     four.split('').forEach(chr => {
         if (dg.indexOf(chr) !== -1) {
@@ -95,45 +92,40 @@ function doMagic(s:string[]):SegmentDecoder {
     });
     sd['d'] = d;
     //...if we have G from DG then D is the other one
-    let g = dg[1-dg.indexOf(d)];
+    let g = otherOne(d,dg);
     sd['g'] = g;
-    //...also if we have 4(BCDF), we can remove CF and D to get B
+    //...also if we have the digit 4(BCDF), we can remove CF and D to get B
     const fourArr:string[] = four.split('');
     let dcf = [d,...cf];
-    dcf.forEach(chr => fourArr.splice(fourArr.indexOf(chr),1));
-    let b = fourArr[0];
+    let b = removeChars(dcf,four);
     sd['b'] = b;
-
 
     // take one of the 6-segment digits (from 0, 6, 9) that DOESN'T have D => it should be 0...
     let zero = '';
-    s.forEach(string => {
-        if ((string.length === 6) && (!string.match(d))) {
+    nLength(s,6).forEach(string => {
+        if (!string.match(d)) {
             zero = string;
         }
     });
     //...and if we remove ABG and CF from 0, the only segment left is E
-    const zeroArr:string[] = zero.split('');
     let abgcf = [a,b,g,...cf];
-    abgcf.forEach(chr => zeroArr.splice(zeroArr.indexOf(chr),1));
-    let e = zeroArr[0];
+    let e = removeChars(abgcf,zero);
     sd['e'] = e;
 
     // take one of the 6-segment digits (from 0, 6, 9) that has all of have ABDEG => it should be 6...
     let six = '';
-    s.forEach(string => {
-        if ((string.length === 6) && string.match(a) && string.match(b) && string.match(d) && string.match(e) && string.match(g)) {
+    nLength(s,6).forEach(string => {
+        if (string.match(a) && string.match(b) && string.match(d) && string.match(e) && string.match(g)) {
             six = string;
         }
     });
     //...and if we remove ABDEG from 6, the only segment left is F
-    const sixArr:string[] = six.split('');
     let abdeg = [a,b,d,e,g];
-    abdeg.forEach(chr => sixArr.splice(sixArr.indexOf(chr),1));
-    let f = sixArr[0];
+    let f = removeChars(abdeg,six);
     sd['f'] = f;
-    //...if we have F from CF then C is the other one
-    let c = cf[1-cf.indexOf(f)];
+
+    //If we have F from CF then C is the other one
+    let c = otherOne(f,cf);
     sd['c'] = c;
 
     return sd;
@@ -141,7 +133,7 @@ function doMagic(s:string[]):SegmentDecoder {
 
 function decodeNumber(s:string[],decipher:DigitDecoder):number {
     //decodes a number from segment descriptions ['afbcd', 'ef', ...] with 'decipher' decoder, and returns corresponding number
-    const sSorted = s.map(onestring => onestring.split('').sort().join(''));
+    const sSorted = s.map(onestring => onestring.split('').sort().join('')); 
     return parseInt(sSorted.map(number => decipher[number]).join(''),10);
 }
 
@@ -167,16 +159,16 @@ console.log(`First part: ${numberOfUniques} unique length segmentlists.`);
 //SOLUTION FOR SECOND PART
 let sum = 0;
 for (let line = 0;line<inputPatterns.length;line++) {
-    const segmentDecoder = doMagic(inputPatterns[line]); 
-    const decipher:DigitDecoder = {};
-    Object.keys(numbers).forEach(key => {
+    const segmentDecoder = doMagic(inputPatterns[line]);    //get the decipher for the current puzzle
+    const decipher:DigitDecoder = {};                       //create a lookup object for the decipher
+    Object.keys(originalNumbers).forEach(key => {
         decipher[
             key.split('')
             .map(c => segmentDecoder[c])
             .sort()
             .join('')
-        ] = numbers[key];
+        ] = originalNumbers[key];
     });
-    sum += decodeNumber(inputDigits[line],decipher);
+    sum += decodeNumber(inputDigits[line],decipher);        //and decode the numbers with it
 }
 console.log("Second part, sum of all numbers:",sum);

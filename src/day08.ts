c363c26687d0b2bfa4f4600354de4db2939878bd
Péter Szász <eszpee@gmail.com>
2021-12-08 21:17:20 +0100

    Almost there

--- src/day08.ts
+++ src/day08.ts
@@ -1,8 +1,5 @@
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day08.txt');
-//['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'];
-//['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | fcgedb cgb dgebacf gc'];
-//readInputArray('../input/day08_sample.txt');
 
 function is1478(s:string):boolean {
     //checks if the passed string represents 1,4,7 or 8 
@@ -10,10 +7,6 @@ function is1478(s:string):boolean {
     const goodLength = new Set([2,4,3,7]);
     return goodLength.has(s.length);
 }
-interface SegmentDecoder {
-    [key: string]: string;
-}
-
 interface DigitDecoder {
     [key: string]: number;
 }
@@ -29,6 +22,11 @@ const originalNumbers:DigitDecoder = {
     'abcdefg': 8,
     'abcdfg': 9
 };
+
+interface SegmentDecoder {
+    [key: string]: string;
+}
+
 function doMagic(s:string[]):SegmentDecoder {
     // deducts which segment represents which one and returns a segment lookup object
     function nLength(s:string[],n:number):string[] {
@@ -36,16 +34,6 @@ function doMagic(s:string[]):SegmentDecoder {
         let r:string[] = new Array();
         s.forEach(i => {if (i.length === n) {r.push(i)}});
         return r;
-    }
-
-    function nonPresent(check:string[],reference?:string):string[] {
-        //checks strings compared to reference
-        //returns the characters that aren't present in any of the strings in 'check' from reference (or [abcdefg])
-        const ref = reference || 'abcdefg';
-        const checkMerged = check.join('');
-        let np:string[] = new Array();
-        ref.split('').forEach(chr => {if (!checkMerged.includes(chr)){np.push(chr);}});
-        return np;
     }
 
     function otherOne(first:string,pair:string[]):string { 
@@ -61,26 +49,25 @@ function doMagic(s:string[]):SegmentDecoder {
     }
 
     let sd:SegmentDecoder = {};
-
     s = s.map(x=>x.split('').sort().join(''));
 
-    const cf = nLength(s,2)[0].split(''); // the only 2-segment number is 1, so we have C or F
+    // the only 2-segment number is 1, so we have C or F
+    const cf = nLength(s,2)[0].split(''); 
 
-    const a = nonPresent(cf,nLength(s,3)[0])[0];
-    sd['a'] = a; // the only 3-segment number is 7, that includes C and F, so the remainder is A
+    // the only 3-segment number is 7, that includes C and F, so the remainder is A
+    const a = removeChars(cf,nLength(s,3)[0]);
+    sd['a'] = a; 
 
     //get the string that's 5-char long and includes ACF... 
-    let three = '';
-    let acf = [a,...cf];
-    nLength(s,5).forEach(string => {
+    let three = nLength(s,5).filter(string => {
         let contains = 0;
-        acf.forEach(chr => {if (string.match(chr)){contains++}});
+        [a,...cf].forEach(chr => {if (string.match(chr)){contains++}});
         if (contains === 3) {
-            three = string;
+            return true;
         }
-    });
+    })[0];
     //... and if we remove ACF then we have DG
-    let dg = removeChars(acf,three).split('');
+    let dg = removeChars([a,...cf],three).split('');
 
     //get the string that's 4-char long (only one:4) and check which one of DG isn't in it => that should be D
     const four = nLength(s,4)[0];
@@ -96,8 +83,7 @@ function doMagic(s:string[]):SegmentDecoder {
     sd['g'] = g;
     //...also if we have the digit 4(BCDF), we can remove CF and D to get B
     const fourArr:string[] = four.split('');
-    let dcf = [d,...cf];
-    let b = removeChars(dcf,four);
+    let b = removeChars([d,...cf],four);
     sd['b'] = b;
 
     // take one of the 6-segment digits (from 0, 6, 9) that DOESN'T have D => it should be 0...
@@ -108,8 +94,7 @@ function doMagic(s:string[]):SegmentDecoder {
         }
     });
     //...and if we remove ABG and CF from 0, the only segment left is E
-    let abgcf = [a,b,g,...cf];
-    let e = removeChars(abgcf,zero);
+    let e = removeChars([a,b,g,...cf],zero);
     sd['e'] = e;
 
     // take one of the 6-segment digits (from 0, 6, 9) that has all of have ABDEG => it should be 6...
@@ -120,11 +105,10 @@ function doMagic(s:string[]):SegmentDecoder {
         }
     });
     //...and if we remove ABDEG from 6, the only segment left is F
-    let abdeg = [a,b,d,e,g];
-    let f = removeChars(abdeg,six);
+    let f = removeChars([a,b,d,e,g],six);
     sd['f'] = f;
 
-    //If we have F from CF then C is the other one
+    //if we have F from CF then C is the other one
     let c = otherOne(f,cf);
     sd['c'] = c;
 

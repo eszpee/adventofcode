b0a200b840b447be503ae720d97e90812672658a
Péter Szász <eszpee@gmail.com>
2021-12-08 12:04:51 +0100

    day 8 part two with sample in original description. main function to deduct segments missing.

--- src/day08.ts
+++ src/day08.ts
@@ -1,5 +1,7 @@
 import { readInputArray } from "./inputfile";
-const inputArray:string[] = readInputArray('../input/day08.txt');
+const inputArray:string[] = ['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'];
+//['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'];
+//readInputArray('../input/day08.txt');
 const inputPatterns:string[][] = new Array();
 const inputDigits:string[][] = new Array();
 inputArray.forEach(oneline => {
@@ -16,7 +18,6 @@ function is1478(s:string):boolean {
     return goodLength.has(s.length);
 }
 
-//console.log(inputPatterns,inputDigits);
 let numberOfUniques = 0;
 inputDigits.forEach(oneline => {
     oneline.forEach(oneitem => {
@@ -24,3 +25,47 @@ inputDigits.forEach(oneline => {
     })
 });
 console.log(`${numberOfUniques} unique length segmentlists.`);
+
+const numbers = {
+    'ABCEFG': 0, 
+    'CF': 1,
+    'ACDEG': 2,
+    'ACDFG': 3,
+    'BCDF': 4,
+    'ABDFG': 5,
+    'ABDEFG': 6,
+    'ACF': 7,
+    'ABCDEFG': 8,
+    'ABCDFG': 9,
+};
+
+function doMagic(s:string[]) {
+    // deducts which segment represents which one 
+    let sd = { //TODO: this will need to be filled up runtime
+        'd': 'A',
+        'e': 'B',
+        'a': 'C',
+        'f': 'D',
+        'g': 'E',
+        'b': 'F',
+        'c': 'G',
+    };
+    return sd;
+}
+
+function decodeNumber(s:string[],d):number {
+    //decodes a number from segment descriptions ['afbcd', 'ef', ...] with d decoder, and returns corresponding number
+    let decodedNumber = s
+        .map(onestring => onestring.split('')
+            .map(onechar => d[onechar])
+                .sort().join('')
+        ).map(onestring => numbers[onestring])
+            .join('');
+    return parseInt(decodedNumber,10);
+}
+
+//const segmentDecoder = doMagic(inputPatterns[0]); //TODO: this should be a loop
+
+let finalNumbers:number[] = inputDigits.map((digit,index) => decodeNumber(digit,doMagic(inputPatterns[index])));
+console.log(finalNumbers);
+

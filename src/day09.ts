e55e8770a41e37ddf500c9d91be2c46c2cdf194f
Péter Szász <eszpee@gmail.com>
2021-12-09 11:00:30 +0100

    Cleanup

--- src/day09.ts
+++ src/day09.ts
@@ -1,8 +1,8 @@
-import { debug } from "console";
 import { readInputArray } from "./inputfile";
 const inputArray:string[] = readInputArray('../input/day09.txt');
 
  function firstPart(map:number[][]):number {
+
     function isMin(center:number, top:number, bottom:number, left:number, right:number):boolean {
         return ((center<top) && (center<bottom) && (center<left) && (center<right));
     }
@@ -22,6 +22,7 @@ const inputArray:string[] = readInputArray('../input/day09.txt');
     return minSum;
 }
 
+
 function secondPart(inputMap:number[][]):number {
 
     function numToBitmap(numMap:number[][]):boolean[][] {
@@ -36,66 +37,38 @@ function secondPart(inputMap:number[][]):number {
         return returnMap;
     }
 
-    function printBitmap(bitMap:boolean[][]):void {
-        let s = '';
-        for (let y=0;y<bitMap.length;y++) {    
-            for (let x=0;x<bitMap[y].length;x++) {
-                if (bitMap[y][x]) { s+='░';}
-                else {s+='█';}
-            }
-            s+='\n';
-        }
-        console.log(s.substring(0, s.length - 1));
-    }
-
     function floodFill(x:number,y:number,currSize:number = 0):number {
-        // TODO: shouldn't count false areas to area size!
         // Implements flood fill algorythm on bitMap, starting from position (x;y), as long as there are 'true' items around it.
         // returns the size of the area that was filled.
-        const LOG = false;
-        if(LOG) console.log(`floodFill ${x};${y}, currSize`,currSize);
         if (bitMap[y][x]) {
             //current point can be filled
             bitMap[y][x] = false;
             currSize++; 
-            if(LOG) console.log("point is empty, can be filled");
-            if(LOG) printBitmap(bitMap);
-            if(LOG) console.log(currSize);
 
             //check point above
             if (y>0) {
-                if(LOG) console.log("checking point above");
                 currSize = floodFill(x,y-1,currSize);
             } 
 
             //check point below
             if (y<bitMap.length-1) {
-                if(LOG) console.log("checking point below");
                 currSize = floodFill(x,y+1,currSize);
             } 
 
             //check point left
             if (x>0) {
-                if(LOG) console.log("checking point left");
                 currSize = floodFill(x-1,y,currSize);
             } 
 
             //check point right
             if (x<bitMap[0].length-1) {
-                if(LOG) console.log("checking point right");
                 currSize = floodFill(x+1,y,currSize);
             } 
-        }
-        else {
-            if(LOG) console.log("point already filled");
-            return currSize;
         }
         return currSize;
     }
 
     const bitMap = numToBitmap(inputMap)
-    if(false) console.log("map before floodfill");
-    if(false) printBitmap(bitMap);
 
     let sizeArr:number[] = new Array;
     for (let y=0;y<bitMap.length;y++) {    

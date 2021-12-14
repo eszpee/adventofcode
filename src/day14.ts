137e9bb23f17663cda56250679aab6795faf77d4
Péter Szász <eszpee@gmail.com>
2021-12-14 15:14:30 +0100

    First part without string calculation (lanternfish PTSD)

--- src/day14.ts
+++ src/day14.ts
@@ -16,18 +16,19 @@ BN -> B
 BB -> N
 BC -> B
 CC -> N
-CN -> C`.split('\n'); */
+CN -> C`.split('\n'); 
+*/
 readInputArray('../input/day14.txt'); 
+
+function inc(a:number|undefined,b:number=1):number {
+    if (a) { return a+b; } else {return b;}
+}
 interface Dict {
     [key: string]: string;
 };
 interface Freq {
     [key: string]: number;
 };
-
-function pairInsert(letters:string,rules:Dict):string {
-    return letters.charAt(0)+rules[letters]+letters.charAt(1);
-}
 
 function frequencies(str:string):Freq {
     let freq:Freq = {};
@@ -42,8 +43,29 @@ function frequencies(str:string):Freq {
     return freq;
 }
 
-let str:string = inputArray.shift() || '';
-inputArray.shift();
+function pairsToSingles(pairs:Freq,first:string,last:string):Freq {
+    //returns single character frequencies from pairs
+    let freq:Freq = {};
+    Object.keys(pairs).forEach(pair =>{
+        freq[pair.charAt(0)] = inc(freq[pair.charAt(0)],pairs[pair]);
+        freq[pair.charAt(1)] = inc(freq[pair.charAt(1)],pairs[pair]);
+    });
+    //add 1 to ends...
+    freq[first]++;
+    freq[last]++;
+    //...and divide everything by 2 because we counted all chars twice
+    Object.keys(freq).forEach(letter =>{
+        freq[letter] = freq[letter] / 2;
+    });
+    return freq;
+}
+
+
+const inputString:string = inputArray.shift() || '';
+const first = inputString.charAt(0);
+const last = inputString.charAt(inputString.length-1);
+
+inputArray.shift(); //empty line separator in input file
 
 const dictionary:Dict = {};
 inputArray.forEach(cmd => {
@@ -51,22 +73,30 @@ inputArray.forEach(cmd => {
     dictionary[fromTo[0]] = fromTo[1];
 });
 
-new Array(10).fill(undefined).forEach(cmd => {
-    let newString:string = '';
-    for(let pos=0;pos<str.length-1;pos++) {
-        const pairToCheck = str.charAt(pos)+str.charAt(pos+1);
-        // last character should be removed unless we're at the last iteration
-        if (pos === str.length-2) {
-            newString += pairInsert(pairToCheck,dictionary);
-        }
-        else {
-            newString += pairInsert(pairToCheck,dictionary).slice(0, -1); 
-        }
-    }
-    str = newString;
-});
+//initialize pairs and singles
+let pairs:Freq = {};
+for (let i=0;i<inputString.length-1;i++) {
+    pairs[inputString.charAt(i)+inputString.charAt(i+1)] = inc(pairs[inputString.charAt(i)+inputString.charAt(i+1)]);
+}
+let singles:Freq = pairsToSingles(pairs,first,last);
 
-const freq = frequencies(str);
-const freqValuesSorted = Object.values(freq).sort(function(a,b){return b-a});
-console.log("Solution to first part:",(freqValuesSorted.shift()||0)-(freqValuesSorted.pop()||0));
+//loop n times
+for (let steps=1;steps<=10;steps++) {
+    let newPairs:Freq = {};
+    Object.keys(pairs).forEach(pair =>{
+        //create new pairs
+        const c1 = pair.charAt(0);
+        const c2 = dictionary[pair];
+        const c3 = pair.charAt(1);
+        newPairs[c1+c2] = inc(newPairs[c1+c2],pairs[pair]);
+        newPairs[c2+c3] = inc(newPairs[c2+c3],pairs[pair]);
+    });
+    pairs = newPairs;
+    console.log(steps,". step, pairs after step:",pairs);
+    singles = pairsToSingles(pairs,first,last);
+    console.log("Singles:",singles);    
+};
+
+const singlesSorted = Object.values(singles).sort(function(a,b){return b-a});
+console.log("Solution to first part:",(singlesSorted.shift()||0)-(singlesSorted.pop()||0));
 

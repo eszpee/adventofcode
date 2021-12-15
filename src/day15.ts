018781d9d44c320d05906f22b6fa046f21433f97
Péter Szász <eszpee@gmail.com>
2021-12-15 20:50:00 +0100

    naive solution last iteration

--- src/day15.ts
+++ src/day15.ts
@@ -2,9 +2,9 @@ const start = new Date();
 import { readInputArray } from "./inputfile";
 import { inspect } from 'util';
 
-const ITERMAX=100000000;
-const MAXLENGTH=2.5;//path max length is 3x map width
-let inputArray:string[] =  //readInputArray('../input/day15.txt'); /*
+const ITERMAX=Infinity;//100000000;
+const MAXLENGTH=5;//path max length is n times map width
+let inputArray:string[] =  // readInputArray('../input/day15.txt'); /*
  [
     '1163751742',
     '1381373672',
@@ -34,29 +34,45 @@ for (let i=0;i<mapSize;i++) {
     easyPath += origMap[0][i];
 }
 easyPath -= origMap[0][0];
-const emptyMap:boolean[][] = new Array(mapSize).fill(undefined).map(undefined => new Array(mapSize).fill(false));
-
 class toDo {
     startCoord: number[]; 
-    visitedSoFar: boolean[][];
+    visitedSoFar: number[][];
     sumSoFar: number;
-    constructor(start:number[],sumSoFar:number,visitedSoFar:boolean[][]) {
+    constructor(start:number[],sumSoFar:number,visitedSoFar:number[][]) {
         this.startCoord = start;
-        this.sumSoFar = 0;
-        this.visitedSoFar = emptyMap;
+        this.sumSoFar = sumSoFar;
+        this.visitedSoFar = visitedSoFar;
     }
 
 }
 
-function pathLength(map:boolean[][]):number {
-    return map.flat(2).filter(x=>x).length;
+function ArrIncl(arr:number[][], subarr:number[]){
+    for(var i = 0; i<arr.length; i++){
+        let checker = false
+        for(var j = 0; j<arr[i].length; j++){
+            if(arr[i][j] === subarr[j]){
+                checker = true
+            } else {
+                checker = false
+                break;
+            }
+        }
+        if (checker){
+            return true
+        }
+    }
+    return false
 }
 
-function drawVisited(visitedMap:boolean[][]):string {
+function pathLength(map:number[][]):number {
+    return map.length;
+}
+
+function drawVisited(visitedMap:number[][]):string {
     let canvas = '';
-    for (let i=0;i<visitedMap.length;i++) {
-        for (let j=0;j<visitedMap.length;j++) {
-            if (visitedMap[i][j]) { canvas += 'X'} else {canvas += '.'}
+    for (let i=0;i<mapSize;i++) {
+        for (let j=0;j<mapSize;j++) {
+            if (ArrIncl(visitedMap,[i,j])) { canvas += 'X'} else {canvas += '.'} 
         }
         canvas += '\n';
     }
@@ -64,38 +80,33 @@ function drawVisited(visitedMap:boolean[][]):string {
 }
 
 let stack:toDo[] = new Array();
-stack.push(new toDo([0,0],0,emptyMap)); //let's start from top left corner
+stack.push(new toDo([0,0],0,[])); //let's start from top left corner
 
 const goodPaths:number[] = new Array();
 let steps = 1;
 
 while ((stack.length>0) && (steps <= ITERMAX)) {
     const bestPath = goodPaths.sort((a,b) => a-b)[0] || easyPath;
-    if (steps % 10000 === 0) {
+    if (steps % 100000 === 0) {
+        //sort stack 
+        stack = stack.sort((a,b) => (a.sumSoFar) - (b.sumSoFar));
         console.log('-----STEP',steps);
         console.log('Stack size:',stack.length, "Shortest path:",bestPath);
-        /*         console.log("path taken:");
-        console.log(drawVisited(stack[stack.length-1].visitedSoFar));
-        console.log("length:");
-        console.log(pathLength(stack[stack.length-1].visitedSoFar));
- */    };
+
+//        console.log(drawVisited(stack[stack.length-1].visitedSoFar));
+        console.log("length:",pathLength(stack[stack.length-1].visitedSoFar));
+   };
 //    console.log("stack length and contents before pop()",stack.length,inspect(stack,{depth: 3, colors: true}))
     steps++;
 
-    //sort stack 
-/*     if (steps % 1000 === 0) { 
-        console.log("Sorting stack by favoring smallest chain value / length to optimize further")
-        stack = stack.sort((a,b) => (b.sumSoFar/pathLength(b.visitedSoFar)) - (a.sumSoFar-pathLength(a.visitedSoFar)));
-    };
- */
     // let currToDo = JSON.parse(JSON.stringify(stack.pop()));
     let currToDo = Object.assign({}, stack.pop());
     let newSum = currToDo?.sumSoFar || 0;
     let visitedMap = currToDo?.visitedSoFar;
     const x =  currToDo?.startCoord[0];
     const y =  currToDo?.startCoord[1];
-    if (!visitedMap[x][y] && (newSum <= bestPath-origMap[mapSize-1][mapSize-1]) && (pathLength(visitedMap) <= mapSize*MAXLENGTH)) { 
-        visitedMap[x][y] = true;
+    if (!ArrIncl(visitedMap,[x,y]) && (newSum <= bestPath-origMap[mapSize-1][mapSize-1])) {// && (pathLength(visitedMap) <= mapSize*MAXLENGTH)) { 
+        visitedMap.push([x,y]);
         newSum += origMap[x][y];
         if ((x===mapSize-1) && (y===mapSize-1)) {
             console.log("arrived to end at step",steps,"sum:",newSum);
@@ -105,15 +116,15 @@ while ((stack.length>0) && (steps <= ITERMAX)) {
         }    
         else {
             let newTodoList = new Array();
-            let newTodo = new toDo([0,0],0,emptyMap);
+            let newTodo = new toDo([0,0],0,[]);
             let objToPush;
             newTodo.sumSoFar = newSum;
             newTodo.visitedSoFar = visitedMap;
 
-             //up
+/*              //up
              if (x>0) {
-                if (!visitedMap[x-1][y]) {
-                    let up = new toDo([x-1,y],0,emptyMap);
+                if (!ArrIncl(visitedMap,[x-1,y])) {
+                    let up = new toDo([x-1,y],0,[]);
                     up.sumSoFar = newSum;
                     up.visitedSoFar = visitedMap;
                     newTodoList.push(up);
@@ -121,23 +132,23 @@ while ((stack.length>0) && (steps <= ITERMAX)) {
             }
             //left
             if (y>0) {
-                if (!visitedMap[x][y-1]) {
-                    let left = new toDo([x,y-1],0,emptyMap);
+                if (!ArrIncl(visitedMap,[x,y-1])) {
+                    let left = new toDo([x,y-1],0,[]);
                     left.sumSoFar = newSum;
                     left.visitedSoFar = visitedMap;
                     newTodoList.push(left);
                 }
             }
-            //right
+ */            //right
             if (y<mapSize-1) {
-                if (!visitedMap[x][y+1]) {
+                if (!ArrIncl(visitedMap,[x,y+1])) {
                     newTodo.startCoord = [x,y+1];
                     newTodoList.push(JSON.parse(JSON.stringify(newTodo)));
                 }
             }
             //down
             if (x<mapSize-1) {
-                if (!visitedMap[x+1][y]) {
+                if (!ArrIncl(visitedMap,[x+1,y])) {
                     newTodo.startCoord = [x+1,y];
                     newTodoList.push(newTodo);
                 }

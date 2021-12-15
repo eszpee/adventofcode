afc91f7851b91e899ae8d840011796beb1fd3cf5
Péter Szász <eszpee@gmail.com>
2021-12-15 22:26:36 +0100

    part 2 sample solved

--- src/day15.js
+++ src/day15.js
@@ -5,7 +5,7 @@ const inputfile = require("./inputfile");
 
 const inspect = require('util');
 
-const inputArray =  inputfile.readInputArray('../input/day15.txt'); /*
+const inputArray = //  inputfile.readInputArray('../input/day15.txt'); /*
  [
     '1163751742',
     '1381373672',
@@ -19,23 +19,63 @@ const inputArray =  inputfile.readInputArray('../input/day15.txt'); /*
     '2311944581',
 ]; //*/
 
-const mapSize = inputArray.length;
+function solve(input) {
+    const inputArray = input;
+    const mapSize = inputArray.length;
 
-const graph = {}
-const nums = inputArray.map(line => line.split('').map(Number));
-for (let x=0;x<mapSize;x++) {
-    for (let y=0;y<mapSize;y++){
-        let index = 'x'+x+'y'+y;
-        let value = {}
-        if (x < mapSize - 1) {value['x'+parseInt(x+1,10)+'y'+y] = nums[x+1][y]};
-        if (y < mapSize - 1) {value['x'+x+'y'+parseInt(y+1,10)] = nums[x][y+1]};
-        if (x > 0) {value['x'+parseInt(x-1,10)+'y'+y] = nums[x-1][y]};
-        if (y > 0) {value['x'+x+'y'+parseInt(y-1,10)] = nums[x][y-1]};
-        graph[index] = value;
+    const graph = {}
+    const nums = inputArray.map(line => line.split('').map(Number));
+    for (let x=0;x<mapSize;x++) {
+        for (let y=0;y<mapSize;y++){
+            let index = 'x'+x+'y'+y;
+            let value = {}
+            if (x < mapSize - 1) {value['x'+parseInt(x+1,10)+'y'+y] = nums[x+1][y]};
+            if (y < mapSize - 1) {value['x'+x+'y'+parseInt(y+1,10)] = nums[x][y+1]};
+            if (x > 0) {value['x'+parseInt(x-1,10)+'y'+y] = nums[x-1][y]};
+            if (y > 0) {value['x'+x+'y'+parseInt(y-1,10)] = nums[x][y-1]};
+            graph[index] = value;
+        }
     }
+    const endCoord = 'x'+parseInt(mapSize-1,10)+'y'+parseInt(mapSize-1,10);
+    const shortestPath = findShortestPathWithLogs(graph, 'x0y0', endCoord);
+    console.log(shortestPath);
 }
-const endCoord = 'x'+parseInt(mapSize-1,10)+'y'+parseInt(mapSize-1,10);
-const shortestPath = findShortestPathWithLogs(graph, 'x0y0', endCoord);
-console.log(shortestPath);
+//solve(inputArray);
+
+function incDigit(a,b=1) {
+    let res = a+b;
+    if (res > 9) { res -= 9;}
+    return res;
+}
+function incLine5(s) {
+    let newS = s;
+    let nums = s.split('').map(Number);
+    newS += nums.map(d => incDigit(d)).join('');
+    nums = nums.map(d => incDigit(d));
+    newS += nums.map(d => incDigit(d)).join('');
+    nums = nums.map(d => incDigit(d));
+    newS += nums.map(d => incDigit(d)).join('');
+    nums = nums.map(d => incDigit(d));
+    newS += nums.map(d => incDigit(d)).join('');
+    return newS;
+}
+
+function fiveRows(s) {
+    let returnRows = [];
+    for (let i=0;i<5;i++) {
+        for (let j=0;j<s.length;j++) {
+            nums = s[j].split('').map(Number);
+            returnRows.push(nums.map(d => incDigit(d,i)).join(''));
+        }
+    }
+    return returnRows;
+}
+
+
+let newInput = fiveRows(inputArray).map(line => incLine5(line));
+
+console.log(newInput);
+solve(newInput);
+
 const end = new Date() - start;
 console.log('Execution time: %dms', end)

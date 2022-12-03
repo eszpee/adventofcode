b46a19f15900340335577ca233ffde18b8eec0a0
Péter Szász <peter.szasz@kinja.com>
2022-12-03 15:08:39 +0000

    day 3 part 2 sample

--- 2022/day3.js
+++ 2022/day3.js
@@ -1,6 +1,5 @@
-const inputArray = readInput("./day3.txt");
-//console.log(inputArray);
-/*
+const is = require("lodash"); // Import the lodash library
+const inputArray = //readInput("./day3.txt"); /*
  [
     'vJrwpWtwJgWrhcsFMMfFFhFp',
     'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
@@ -16,7 +15,16 @@ for (var i=0;i<inputArray.length;i++) {
     sum += getPriority(commonLetters(halfen(inputArray[i])[0],halfen(inputArray[i])[1])[0]);
 }
 
-console.log(sum);
+console.log("First part: ",sum);
+
+sum = 0;
+for (var i=0;i<inputArray.length;i++) {
+  sum += getPriority(common3(inputArray[i],inputArray[i+1],inputArray[i+2])[0]);
+  i+=2;
+}
+
+console.log("Second part: ",sum);
+
 
 function halfen(s) {
     //returns an array of two strings, input split to two parts
@@ -32,9 +40,9 @@ function commonLetters(str1, str2) {
   
     // Return the array of common characters
     return common;
-  }
+}
 
-  function getPriority(ch) {
+function getPriority(ch) {
     // Check if the character is a lowercase letter
     if (ch >= "a" && ch <= "z") {
       // Return the priority of the lowercase letter
@@ -49,7 +57,21 @@ function commonLetters(str1, str2) {
     else {
       return 0;
     }
-  }
+}
+
+
+function common3(str1, str2, str3) {
+  // Split the strings into arrays of characters
+  var chars1 = str1.split("");
+  var chars2 = str2.split("");
+  var chars3 = str3.split("");
+
+  // Find the intersection of the three arrays of characters
+  var common = is.intersection(chars1, chars2, chars3);
+
+  // Return the array of common characters
+  return common;
+}
 
 function readInput(filename) {
   const fs = require('fs');

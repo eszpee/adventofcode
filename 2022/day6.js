dc9f7b1131a832b4406112ba7dd6f3bb5034b4ab
Péter Szász <peter.szasz@kinja.com>
2022-12-06 06:24:21 +0000

    sample done

--- 2022/day6.js
+++ 2022/day6.js
@@ -1,9 +1,41 @@
-const inputArray = //readInput("./day4.txt"); /*
- [
-'bvwbjplbgvbhsrlpgdmjqwftvncz',
-]; //*/
+const input = //readInput("./day6.txt"); /*
+// [
+'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw';
+//]; //*/
+
+//iterate pointer over string
+//console.log(fourDifferent('abta'));
+//console.log(fourDifferent('kepz'));
+
+var pointer = 3;
+
+while (true) {
+  var substr = input.charAt(pointer-3)+input.charAt(pointer-2)+input.charAt(pointer-1)+input.charAt(pointer);
+  pointer++;
+  if (fourDifferent(substr)) {
+    console.log('first part:',pointer);
+    break;
+  }
+}
 
 
+function fourDifferent(str) {
+//takes four characters
+//returns true if all different, otherwise false
+  var chars = new Array();
+  chars = str.split('');
+  chars.sort();
+//  console.log(chars);
+  var alldifferent = true;
+  for (i=1;i<chars.length;i++) {
+    if (chars[i-1] === chars [i]) {
+      alldifferent = false;
+      break;
+    }
+  }
+  return alldifferent;
+
+}
 
 
 function readInput(filename) {

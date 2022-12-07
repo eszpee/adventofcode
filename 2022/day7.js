1ee0f40605959e7b37a3bf5dc1dc46ef5c8edf2e
Péter Szász <peter.szasz@kinja.com>
2022-12-07 21:18:05 +0000

    cleanup

--- 2022/day7.js
+++ 2022/day7.js
@@ -12,7 +12,6 @@ var currentdir = '';
 
 //go over input, fill out files associative array to have sizes for all directories
 input.forEach(line => {
-  //console.log(line);
   switch (line.charAt(0)) {
     case '$':
       //command, cd or ls
@@ -43,27 +42,18 @@ input.forEach(line => {
     default:
       //file in file list
       var [size,filename] = line.split(' ');
-      //console.log(currentdir,filename);
       files[currentdir+filename] = Number(size);
       if (Number(size) == NaN) {exit(1);}
-      //console.log(files);
       var workingdir = currentdir;
-      var circuitbreaker = 5;
       while (workingdir != '/') {
-        //console.log(workingdir);
         files[workingdir] += Number(size);
-        //console.log(files);
         workingdir = workingdir.replace(/[^\\/]+\/$/,'');
-        circuitbreaker--;
-        //if(circuitbreaker == 0) {exit(1);}
       }
       files['/'] += Number(size);
       break;
   }  
-  //console.log(files);
 });
 
-//console.log(files);
 var sum = 0;
 Object.keys(files).forEach(function(key, index) {
   if ((key.match(/\/$/)) && (files[key] <= 100000)) {
@@ -74,7 +64,6 @@ Object.keys(files).forEach(function(key, index) {
 console.log("First part:",sum);
 
 var freespace = 70000000-files['/'];
-console.log("Free space",freespace);
 var needed = 30000000-freespace;
 
 var smallest = 70000000;

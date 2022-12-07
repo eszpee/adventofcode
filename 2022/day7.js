12996400674664df5647247d0495b9089febd253
Péter Szász <peter.szasz@kinja.com>
2022-12-07 20:35:28 +0000

    sample almost done (need to solve for root dir)

--- 2022/day7.js
+++ 2022/day7.js
@@ -5,7 +5,7 @@ const input = readInput("./day7-sample.txt"); /*
 
 // data structure:
 //files{'/dirname':123}
-var files =  new Object();
+var files =  new Object;
 var currentdir = '';
 
 //go over input, fill out files associative array to have sizes for all directories
@@ -13,17 +13,40 @@ input.forEach(line => {
   switch (line.charAt(0)) {
     case '$':
       //command, cd or ls
-
+      if (line.charAt(2) == 'c') {
+        //command is cd
+        var newdir = line.substring(5);
+        switch (newdir) {
+          case '/':
+            files['/'] = 0;
+            currentdir = '/';
+            break;
+          case '..':
+            currentdir = currentdir.replace(/[a-z]\/$/,'');
+            break;          
+          default: //any new dir
+            currentdir += newdir+'/';
+            break;
+        }
+      }
       break;
   
     case 'd':
       //directory in file list
-
+      var dirname = currentdir + line.split(' ')[1] + '/';
+      files[dirname] = 0;
       break;
 
     default:
       //file in file list
-
+      var [size,filename] = line.split(' ');
+      files[currentdir+filename] = Number(size);
+      var workingdir = currentdir;
+      while (workingdir != '/') {
+        files[workingdir] += Number(size);
+        workingdir = workingdir.replace(/[a-z]\/$/,'');
+      }
+      //TODO: add the same size to all parent directories
       break;
   }
   

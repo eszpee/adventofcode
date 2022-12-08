1f9b4fe547b366a50a3488aca4b3fcda9bd6bbad
Péter Szász <peter.szasz@kinja.com>
2022-12-08 07:58:36 +0000

    second part sample done

--- 2022/day8.js
+++ 2022/day8.js
@@ -11,41 +11,98 @@ forest['123','345','453'];
 
 var visible=(input[0].length-1)*4;          //edges
 
+var scenemax = 0;
 for (var y=1;y<input.length-1;y++) {        //only checking within edges
   for (var x=1;x<input[y].length-1;x++) {   //maybe not square
-    //console.log(input[y].charAt(x));
-      if (seen(x,y)) { console.log('v');visible++; }
+      if (seen(x,y)) { visible++; }
+      var scenic = scene(x,y);
+      if (scenemax < scenic) {scenemax = scenic;}
   }
 }
+
+console.log("First part:",visible);
+console.log("Second part:",scenemax);
+
+
+function scene(startX,startY) {
+  //takes coordinate
+  //calculates scenic score for tree, by checking all directions and multiplying the trees visible 
+  //until reaching one that's higher or the same as tree
+  var tree = Number(input[startY].charAt(startX));
+  var all = 1;
+  var count = new Array();
+  //console.log('- checking for',startX,startY,tree);
+
+  //console.log('checking up');
+  count[0] = 1;
+  for (var y=startY-1;y>0;y--) {
+    //console.log('y',y);
+    //console.log('comparing',input[y].charAt(startX),'with',tree);
+    if (input[y].charAt(startX) < tree) { count[0]++;}
+    else {break;}
+  }
+  console.log('visible trees:',count[0]);
+
+  //console.log('checking left');
+  count[1] = 1;
+  for (var x=startX-1;x>0;x--) {
+    //console.log('y',y);
+    //console.log('comparing',input[y].charAt(startX),'with',tree);
+    if (input[startY].charAt(x) < tree) { count[1]++;}
+    else {break;}
+  }
+  //console.log('visible trees:',count[1]);
+
+  //console.log('checking right');
+  count[2] = 1;
+  for (var x=startX+1;x<input[startY].length-1;x++) {
+    //console.log('y',y);
+    //console.log('comparing',input[startY].charAt(x),'with',tree);
+    if (input[startY].charAt(x) < tree) { count[2]++;}
+    else {break;}
+  }
+  //console.log('visible trees:',count[2]);
+
+  //console.log('checking down');
+  count[3] = 1;
+  for (var y=startY+1;y<input.length-1;y++) {
+    //console.log('y',y);
+    //console.log('comparing',input[y].charAt(startX),'with',tree);
+    if (input[y].charAt(startX) < tree) { count[3]++;}
+    else {break;}
+  }
+  //console.log('visible trees:',count[3]);
+
+  all = count[0]*count[1]*count[2]*count[3];
+  console.log('area size',all);
+  return all;
+}
+
+
 
 function seen(startX,startY) {
   //takes coordinate
   //returns true if tree is seen from edge in any direction
   var tree = Number(input[startY].charAt(startX));
-  console.log('checking for',startX,startY,tree);
 
-  console.log('checking from up');
   var max = 0;
   for (var y=0;y<startY;y++) {
     if (input[y].charAt(startX) > max) { max = input[y].charAt(startX);}
   }
   if (max < tree) { return true; }
 
-  console.log('checking from down');
   var max = 0;
   for (var y=startY+1;y<input.length;y++) {
     if (input[y].charAt(startX) > max) { max = input[y].charAt(startX);}
   }
   if (max < tree) { return true; }
 
-  console.log('checking from left');
   max = 0;
   for (var x=0;x<startX;x++) {
     if (input[startY].charAt(x) > max) { max = input[startY].charAt(x);}
   }
   if (max < tree) { return true; }
 
-  console.log('checking from right');
   max = 0;
   for (var x=startX+1;x<input[startY].length;x++) {
     if (input[startY].charAt(x) > max) { max = input[startY].charAt(x);}
@@ -55,9 +112,9 @@ function seen(startX,startY) {
   return false;
 }
 
-console.log("First part:",visible);
 
-//console.log("Second part:",visible);
+
+
 
 
 function readInput(filename) {

4ecac08d714968d7b7fdf771d6b0dd2bb764d5b7
Péter Szász <peter.szasz@kinja.com>
2022-12-09 08:09:14 +0000

    part 2 sample working, input breaks, let's visualize rope as next step to debug

--- 2022/day9.js
+++ 2022/day9.js
@@ -1,4 +1,4 @@
-const input = readInput("./day9.txt"); /*
+const input = readInput("./day9-sample.txt"); /*
 
 /* data structures:
 rope[]{x: 1, y: 3}                          //we need array of knots - a full rope, where rope[0] is the head
@@ -6,7 +6,7 @@ visited: Set('1-3')                         //coordinates of visited places
 */ 
 
 var rope = new Array()
-for (var i=0;i<2;i++) {
+for (var i=0;i<10;i++) {
   rope[i] = {x: 1000, y:1000};              //don't want to think about negatives
 }
 
@@ -18,39 +18,39 @@ input.forEach(line => {
       case 'R':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].x++;  
-          followHead(0);    
+          for (var k = 0; k<9; k++) {followHead(k);}
         }
         break;
       case 'L':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].x--;
-          followHead(0);    
+          for (var k = 0; k<9; k++) {followHead(k);}
         }
         break;
       case 'U':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].y++;
-          followHead(0);    
+          for (var k = 0; k<9; k++) {followHead(k);}
         }
         break;
       case 'D':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].y--;
-          followHead(0);    
+          for (var k = 0; k<9; k++) {followHead(k);}
         }
         break;
       default:
         break;
     }
+    //TODO: visualize rope here.
 });
 
-console.log("First part:",visited.size);
-console.log("Second part:",);
+console.log("Result:",visited.size);
 
 function followHead(h) {
   //h: index of head to follow
   //moves tail after head and records visited coordinates
-  console.log('before move: head:',rope[h],'tail:',rope[h+1]);
+  console.log('checking rope knot',h,'rope before move:', rope);
   if (touching(h)) { console.log('touching');}
   else {
     if (rope[h].x-1 > rope[h+1].x) {
@@ -70,8 +70,8 @@ function followHead(h) {
       rope[h+1].x = rope[h].x;
     }
   }
-  visited.add(rope[h+1].x+'-'+rope[h+1].y);
-  console.log('after move: head:',rope[h],'tail:',rope[h+1]);
+  visited.add(rope[9].x+'-'+rope[9].y);         
+  console.log('after move: ',rope);
 }
 function touching(h){
   //h: head to check if touching

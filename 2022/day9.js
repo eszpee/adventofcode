297b2fe86733f0abc14ebcd6597dbd4fd756dc71
Péter Szász <peter.szasz@kinja.com>
2022-12-09 07:59:47 +0000

    changed first part to use array of knots on a rope instead of head and tail

--- 2022/day9.js
+++ 2022/day9.js
@@ -1,13 +1,15 @@
-const input = readInput("./day9-sample.txt"); /*
+const input = readInput("./day9.txt"); /*
 
 /* data structures:
-head{x: 1, y: 2}
-tail{x: 1, y: 3}
+rope[]{x: 1, y: 3}                          //we need array of knots - a full rope, where rope[0] is the head
 visited: Set('1-3')                         //coordinates of visited places
 */ 
 
-var head = {x: 1000, y:1000};               //don't want to think about negatives
-var tail = {x: 1000, y:1000};
+var rope = new Array()
+for (var i=0;i<2;i++) {
+  rope[i] = {x: 1000, y:1000};              //don't want to think about negatives
+}
+
 var visited = new Set();
 
 input.forEach(line => {
@@ -15,26 +17,26 @@ input.forEach(line => {
     switch (direction) {
       case 'R':
         for (var i=1;i<=Number(steps);i++) {
-          head.x++;
-          followHead();    
+          rope[0].x++;  
+          followHead(0);    
         }
         break;
       case 'L':
         for (var i=1;i<=Number(steps);i++) {
-          head.x--;
-          followHead();    
+          rope[0].x--;
+          followHead(0);    
         }
         break;
       case 'U':
         for (var i=1;i<=Number(steps);i++) {
-          head.y++;
-          followHead();    
+          rope[0].y++;
+          followHead(0);    
         }
         break;
       case 'D':
         for (var i=1;i<=Number(steps);i++) {
-          head.y--;
-          followHead();    
+          rope[0].y--;
+          followHead(0);    
         }
         break;
       default:
@@ -45,36 +47,36 @@ input.forEach(line => {
 console.log("First part:",visited.size);
 console.log("Second part:",);
 
-function followHead() {
-  //no input, uses global head and tail objects. 
+function followHead(h) {
+  //h: index of head to follow
   //moves tail after head and records visited coordinates
-  console.log('before move: head:',head,'tail:',tail);
-  if (touching()) { console.log('touching');}
+  console.log('before move: head:',rope[h],'tail:',rope[h+1]);
+  if (touching(h)) { console.log('touching');}
   else {
-    if (head.x-1 > tail.x) {
-      tail.x++;
-      tail.y = head.y;
+    if (rope[h].x-1 > rope[h+1].x) {
+      rope[h+1].x++;
+      rope[h+1].y = rope[h].y;
     }
-    if (head.x+1 < tail.x) {
-      tail.x--;
-      tail.y = head.y;
+    if (rope[h].x+1 < rope[h+1].x) {
+      rope[h+1].x--;
+      rope[h+1].y = rope[h].y;
     }
-    if (head.y-1 > tail.y) {
-      tail.y++;
-      tail.x = head.x;
+    if (rope[h].y-1 > rope[h+1].y) {
+      rope[h+1].y++;
+      rope[h+1].x = rope[h].x;
     }
-    if (head.y+1 < tail.y) {
-      tail.y--;
-      tail.x = head.x;
+    if (rope[h].y+1 < rope[h+1].y) {
+      rope[h+1].y--;
+      rope[h+1].x = rope[h].x;
     }
   }
-  visited.add(tail.x+'-'+tail.y);
-  console.log('after move: head:',head,'tail:',tail);
+  visited.add(rope[h+1].x+'-'+rope[h+1].y);
+  console.log('after move: head:',rope[h],'tail:',rope[h+1]);
 }
-function touching(){
-  //no input, uses global head and tail objects. 
+function touching(h){
+  //h: head to check if touching
   //returns true of head and tail are touching or overlapping
-  return ((Math.abs(head.x-tail.x) < 2) && (Math.abs(head.y-tail.y) < 2));
+  return ((Math.abs(rope[h].x-rope[h+1].x) < 2) && (Math.abs(rope[h].y-rope[h+1].y) < 2));
 }
 
 

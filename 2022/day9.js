fcc344b9936b428230b8d6127d7275323827f31e
Péter Szász <peter.szasz@kinja.com>
2022-12-09 19:38:40 +0000

    sample with visualisation working!

--- 2022/day9.js
+++ 2022/day9.js
@@ -1,4 +1,10 @@
-const input = readInput("./day9-sample.txt"); /*
+const input = readInput("./day9-sample.txt"); 
+const maxknots = 9;
+String.prototype.replaceAt = function(index, replacement) {
+  return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+const {execSync} = require('child_process');
+
 
 /* data structures:
 rope[]{x: 1, y: 3}                          //we need array of knots - a full rope, where rope[0] is the head
@@ -7,7 +13,7 @@ visited: Set('1-3')                         //coordinates of visited places
 
 var rope = new Array()
 for (var i=0;i<10;i++) {
-  rope[i] = {x: 1000, y:1000};              //don't want to think about negatives
+  rope[i] = {x: 0, y:0};              //don't want to think about negatives
 }
 
 var visited = new Set();
@@ -18,66 +24,107 @@ input.forEach(line => {
       case 'R':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].x++;  
-          for (var k = 0; k<9; k++) {followHead(k);}
+          for (var k = 0; k<maxknots; k++) {followHead(k,line+' '+k);}
         }
         break;
       case 'L':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].x--;
-          for (var k = 0; k<9; k++) {followHead(k);}
+          for (var k = 0; k<maxknots; k++) {followHead(k,line+' '+k);}
         }
         break;
       case 'U':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].y++;
-          for (var k = 0; k<9; k++) {followHead(k);}
+          for (var k = 0; k<maxknots; k++) {followHead(k,line+' '+k);}
         }
         break;
       case 'D':
         for (var i=1;i<=Number(steps);i++) {
           rope[0].y--;
-          for (var k = 0; k<9; k++) {followHead(k);}
+          for (var k = 0; k<maxknots; k++) {followHead(k,line+' '+k);}
         }
         break;
       default:
         break;
     }
-    //TODO: visualize rope here.
 });
 
 console.log("Result:",visited.size);
 
-function followHead(h) {
-  //h: index of head to follow
-  //moves tail after head and records visited coordinates
-  console.log('checking rope knot',h,'rope before move:', rope);
-  if (touching(h)) { console.log('touching');}
-  else {
-    if (rope[h].x-1 > rope[h+1].x) {
-      rope[h+1].x++;
-      rope[h+1].y = rope[h].y;
-    }
-    if (rope[h].x+1 < rope[h+1].x) {
-      rope[h+1].x--;
-      rope[h+1].y = rope[h].y;
-    }
-    if (rope[h].y-1 > rope[h+1].y) {
-      rope[h+1].y++;
-      rope[h+1].x = rope[h].x;
-    }
-    if (rope[h].y+1 < rope[h+1].y) {
-      rope[h+1].y--;
-      rope[h+1].x = rope[h].x;
+function drawRope(msg) {
+  const squaresize = 6;
+  console.log('\033[2J');
+  console.log(msg,'\n');
+  var out = new Array();
+  for(var x=0;x<=squaresize;x++) {
+    out[x] = '';
+    for (var y=0;y<=squaresize;y++) {
+      out[x] += '.';
     }
   }
-  visited.add(rope[9].x+'-'+rope[9].y);         
-  console.log('after move: ',rope);
+  for (var i=maxknots;i>=0;i--) {
+    //putting rope[i] to the map
+    const row = squaresize-rope[i].y;
+    const col = rope[i].x;
+    out[row] = out[row].replaceAt(col,i.toString());
+  }
+  console.log(out.join('\n'));
+  execSync('sleep 0.01'); // block process for a second.
+}
+
+function followHead(h,msg) {
+  //h: index of head to follow
+  //msg: message to pass to visualization
+  //moves tail after head and records visited coordinates
+  //console.log('checking rope knot',h,'rope before move:', rope);
+  if (touching(h)) { 
+    msg += ' touching';
+    //console.log('touching');
+  }
+  else {
+    //check if move was only horizontal
+    if (rope[h].y == rope[h+1].y) {
+      if (rope[h].x-1 > rope[h+1].x) {
+        rope[h+1].x++;
+      }
+      else {        
+        rope[h+1].x--;
+      }
+    }
+    //check if move was only vertical
+    else if (rope[h].x == rope[h+1].x) {
+      if (rope[h].y-1 > rope[h+1].y) {
+        rope[h+1].y++;
+      }
+      else {        
+        rope[h+1].y--;
+      }
+    }
+    //ok, move had to be diagonal
+    else {
+      if (rope[h].x > rope[h+1].x) {
+        rope[h+1].x++;
+      } else {
+        rope[h+1].x--;
+      }
+      if (rope[h].y > rope[h+1].y) {
+        rope[h+1].y++;
+      } else {
+        rope[h+1].y--;
+      }
+    }
+  }
+  visited.add(rope[maxknots].x+'-'+rope[maxknots].y);   
+  //console.log('after move: ',rope);
+  drawRope(msg);      
 }
 function touching(h){
   //h: head to check if touching
   //returns true of head and tail are touching or overlapping
   return ((Math.abs(rope[h].x-rope[h+1].x) < 2) && (Math.abs(rope[h].y-rope[h+1].y) < 2));
 }
+
 
 
 function readInput(filename) {

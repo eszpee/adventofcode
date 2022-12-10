9d390bd8b69b4a24a2ef367d5b40e4c31c3d18ee
Péter Szász <peter.szasz@kinja.com>
2022-12-10 06:47:38 +0000

    plan in comments

--- 2022/day10.js
+++ 2022/day10.js
@@ -1,7 +1,21 @@
 const input = readInput("./day10-sample.txt"); 
 /* data structures:
+code[[1, 'noop'],[3,'addx'[6]]]   //processed input, adding run time to all commands, splitting parameters
+                                  //idea is to simply iterate over this, and decrease run time at each cpu cycle, execute at 0
+cpu_cycle                         //counting cpu cycles
+reg_x                             //global value of register x
+hist_x[1,5,6,-2,3]                //value of register x at the index'th cpu cycle
 */ 
 
+/* algo:
+1. prepare the code array from the input
+2. main while until code.length > 0
+    cpu cycle increase
+    get next command
+    if cpu cycle on command is > 0, decrease
+    else remove command from code, execute command, log hist_x
+3. check hist_x at every 20th occurrence
+*/
 
 input.forEach(line => {
 });

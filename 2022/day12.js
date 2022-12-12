0003ec1f9c4e81196b9cafbb62b179785db33d3e
eszpee <eszpee@gmail.com>
2022-12-12 22:15:23 +0100

    fixed sort

--- 2022/day12.js
+++ 2022/day12.js
@@ -45,7 +45,7 @@ const maxY = atlas.length-1;
 var currstep;
 
 while (currstep = paths.shift()) {
-  console.log(currstep.x,currstep.y);
+  console.log('x:',currstep.x,'y:',currstep.y,'l:',currstep.length,'d:',distance([currstep.x,currstep.y],[Epos.x,Epos.y]));
   var found = false;
   //right
   if (currstep.x < maxX && 

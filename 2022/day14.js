f58940fb968146a06c73484d95fa1d1a65ede8ed
eszpee <eszpee@gmail.com>
2022-12-14 20:03:26 +0100

    dat14 init

---
+++ 2022/day14.js
@@ -0,0 +1,27 @@
+const input = readInput("./day14-sample.txt");
+/* data structures:
+
+*/ 
+/* algo:
+
+*/
+
+for (var i = 0; i<input.length;i++) {
+  line = input[i];
+
+}
+
+console.log('First part:',);
+console.log('Second part:',);
+
+function readInput(filename) {
+  const fs = require('fs');
+
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    return data.split(/\r?\n/);
+  } catch (err) {
+    console.error(err);
+  }
+}
+

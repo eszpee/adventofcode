f622b45ca30e06b0c4b94018f11fdeb6337a5074
eszpee <eszpee@gmail.com>
2022-12-14 20:02:02 +0100

    abandon here

--- 2022/day13.js
+++ 2022/day13.js
@@ -10,9 +10,43 @@ for (var i = 0; i<input.length;i++) {
   line = input[i];
 
 }
+var left = string2array('[1]');
+var right = string2array('[2]');
+console.log(compare(left,right));
+function string2array(s) {
+  return eval(s);
+}
 
-console.log('First part:',);
-console.log('Second part:',);
+
+//console.log('First part:',);
+//console.log('Second part:',);
+
+function compare(leftorig,rightorig) {
+  //takes: two arrays
+  //returns: true if they are in order, false if not
+  console.log('comparing',leftorig,'to',rightorig);
+  left = leftorig.shift();
+  right = rightorig.shift();
+  console.log('first items:',left,right);
+  if (left == undefined) { return true;  }
+  if (right == undefined) { return false; }
+  if (isNaN(left) && isNaN(right)) {
+    //both are arrays
+  } 
+  else if (isNaN(left)) {
+    //left is array, right is number
+  }
+  else if (isNaN(right)) {
+    //right is array, left is number
+  }
+  else {
+    //both are numbers
+    if (left < right) {return true;}
+    else if (left > right) {return false;}
+    //need to continue if they are the same
+  }
+}
+
 
 function readInput(filename) {
   const fs = require('fs');

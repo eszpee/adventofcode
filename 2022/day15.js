6c795d5f82cae30185202914751acf53a8050440
Péter Szász <eszpee@gmail.com>
2022-12-15 21:39:18 +0100

    INPUT DONE

--- 2022/day15.js
+++ 2022/day15.js
@@ -1,4 +1,6 @@
-const input = readInput("./day15-sample.txt");
+const input = readInput("./day15.txt");
+const row_to_check = 2000000;
+
 /* data structures:
 
 */ 
@@ -6,15 +8,64 @@ const input = readInput("./day15-sample.txt");
 
 */
 
-sensors = new Array();
+var sensors = new Array();
 for (var i = 0; i<input.length;i++) {
-  sensors.push(input[i].split(':').map(x => x.match(/([\-\d]+)/g)));
+  sensors.push(input[i].split(':').map(x => x.match(/([\-\d]+)/g))-1);
+}
+/*
+#################.###########
+###S#############.###########
+
+###########################
+####B######################
+*/
+var smallestX = Infinity;
+var highestX = -Infinity;
+var dotsToDraw = new Array();
+for (var i=0; i<sensors.length; i++) {
+  var sensorX = Number(sensors[i][0][0]);
+  var sensorY = Number(sensors[i][0][1]);
+  var beaconX = Number(sensors[i][1][0]);
+  var beaconY = Number(sensors[i][1][1]);
+  var distance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
+  if (smallestX > sensorX) { smallestX = sensorX; }
+  if (smallestX > beaconX) { smallestX = beaconX; }
+  if (highestX < sensorX) { highestX = sensorX; }
+  if (highestX < beaconX) { highestX = beaconX; }
+  console.log (sensors[i], distance);
+
+  var cx = '';
+  var dx = '';
+  if (((sensorY < row_to_check) && (sensorY+distance < row_to_check)) ||
+      ((sensorY > row_to_check) && (sensorY-distance > row_to_check))) {
+        ;
+  }
+  else {
+    cx = sensorX-distance + Math.abs(sensorY-row_to_check);
+    dx = sensorX+distance - Math.abs(sensorY-row_to_check);
+  }
+  dotsToDraw.push([cx,dx]);
+  if (highestX < dx) {
+    highestX = dx;
+  }
+  if (smallestX > cx) {
+    smallestX = cx;
+  }
 }
 
+var checkedRow = new Array(highestX-smallestX).fill('.');
 
+for (var i=0; i<dotsToDraw.length; i++) {
+  for (var d=dotsToDraw[i][0]; d<=dotsToDraw[i][1]; d++ ) {logPoint(d);}
+}
 
-console.log('First part:',sensors);
+console.log('First part:',checkedRow.join('').match(/\#/g).length);
 console.log('Second part:',);
+
+function logPoint(x) {
+  x -= smallestX;
+  checkedRow[x] = '#';
+}
 
 function readInput(filename) {
   const fs = require('fs');

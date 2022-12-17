e72d93ce9882ef713ecdb4c185ba6e4c4999f92f
eszpee <eszpee@gmail.com>
2022-12-17 18:46:33 +0100

    input with random is not working but why

--- 2022/day16.js
+++ 2022/day16.js
@@ -17,17 +17,48 @@ valves[
 
 */
 var valves = new Array();
-
+const valveRand = 0.2;
 for (var i = 0; i<input.length;i++) {
   var currValve = new Object();
   line = input[i];
   currValve.rate = Number(line.match(/=(\d+);/)[1]);
   currValve.leads = line.split(/valves? /)[1].split(', ');
+  currValve.closed = true;
   valves [line.match(/Valve ([A-Z]{2})/)[1]] = currValve;
 }
-console.log(valves);
 
-console.log('First part:',);
+var maxSteamout = 0;
+while (maxSteamout < 1){ //1651) {
+  var timer = 1;
+  var room = 'AA';
+  var steamout = 0;
+  while (timer < 30) {
+    //add released steam from all open valves
+    Object.values(valves).forEach(v => {
+      if (!v.closed) {
+        console.log('Open valve:',v);
+        steamout+= v.rate
+        console.log('Steam out',steamout)
+      }
+    });
+    
+    //we might decide to open the valve
+    if (valves[room].closed && valves[room].rate > 0 && (Math.random() > valveRand)) {        
+      timer++;
+      console.log(timer,'Opened valve');
+      valves[room].closed = false;
+    }
+    // we should move to move
+    room = valves[room].leads[[Math.floor(Math.random() * valves[room].leads.length)]];           //TODO randomness!
+    timer++;
+    console.log(timer,'Moved');
+  }
+  if (maxSteamout<steamout) {
+    maxSteamout = steamout;
+    console.log('Highest now:',maxSteamout);
+  }
+}
+console.log('First part:',maxSteamout);
 console.log('Second part:',);
 
 function readInput(filename) {

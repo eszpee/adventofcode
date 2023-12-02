231ee22d5a368f2f910287064809ac7fca56c117
Péter Szász <peter.szasz@kinja.com>
2023-12-02 13:18:34 +0000

    second part sample OK

--- 2023/day01.js
+++ 2023/day01.js
@@ -19,7 +19,49 @@ for (var i = 0; i<input.length;i++) {
 }
 
 console.log('First part:',megaSum);
-console.log('Second part:',);
+
+megaSum = 0;
+
+for (var i = 0; i<input.length;i++) {
+  line = input[i];
+  let currentString = '';
+  const numbers = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
+  let newLine = '';
+//  console.log('Current line:', line);
+  for (let StartPointer = 0; StartPointer < line.length; StartPointer++) {
+    for (let EndPointer = StartPointer + 1; EndPointer <= line.length; EndPointer++) {
+      currentString = line.substring(StartPointer, EndPointer);
+      if (currentString in numbers) {
+        newLine += numbers[currentString];
+        StartPointer = EndPointer;
+      }
+    }
+    newLine += line.substring(StartPointer, StartPointer+1);
+  }
+//  console.log('Line after replacement:', newLine);
+  line = newLine;
+  const numbersOnly = line.replace(/\D/g, "");
+
+// console.log(numbersOnly); 
+  const leftmostNumber = parseInt(numbersOnly.charAt(0));
+  const rightmostNumber = parseInt(numbersOnly.charAt(numbersOnly.length - 1));
+  megaSum += parseInt(leftmostNumber.toString() + rightmostNumber.toString());
+}
+
+
+
+
+console.log('Second part:',megaSum);
 
 function readInput(filename) {
   const fs = require('fs');

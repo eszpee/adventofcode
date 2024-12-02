d16cf1a77ba85efa63b8a1236554f5957e7a4ee5
Péter Szász <eszpee@gmail.com>
2024-12-02 08:40:30 +0100

    part 1 solved

--- aoc2024/src/day02/index.js
+++ aoc2024/src/day02/index.js
@@ -4,8 +4,11 @@ const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  var safes = 0;
+  input.split('\n').forEach(line => {
+    if (isSafe(line)) {safes++;}
+  });
+  return safes;
 };
 
 const part2 = (rawInput) => {
@@ -14,13 +17,49 @@ const part2 = (rawInput) => {
   return;
 };
 
+//function to tell if a line is safe (meaning: values change in one direction and difference is max 3)
+function isSafe(line) {
+  const values = line.split(' ').map(value => parseInt(value));
+  if (values[0] === values[1]) {
+    return false;    
+  }
+  var growing;
+  if (values[0] < values[1]) {
+    growing = true;
+  }
+  if (values[0] > values[1]) {
+    growing = false;
+  }
+  for (var i=0; i<values.length-1; i++) {
+    if (growing) {
+      if ((values[i+1] - values[i] > 3) || (values[i+1] - values[i] < 1)) {
+        return false;
+      }
+    }
+    //not growing
+    else { 
+      if ((values[i] - values[i+1] > 3) || (values[i] - values[i+1] < 1)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9        
+        `,
+        expected: 2,
+      },
     ],
     solution: part1,
   },
